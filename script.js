93ec7258b8e75e01c24a9b76952688f116b0b467
Ibnlotfi91 <ibnlotfi@hotmail.fr>
2020-12-12 19:56:47 +0100

    end of making template

--- script.js
+++ script.js
@@ -6,12 +6,40 @@
         else{
             $(".navbar").removeClass("sticky");
         }
+        if(this.scrollY > 500){
+            $('.scroll-up-btn').addClass("show");
+        }else{
+            $('.scroll-up-btn').removeClass("show");
+        }
     });
+
+    // slide-up scipt
+    $('.scroll-up-btn').click(function(){
+        $('html').animate({scrollTop: 0});
+    }) 
+
     // togle menu/navbar script
     $('.menu-btn').click(function(){
         $('.navbar .menu').toggleClass("active");
         $('.menu-btn i').toggleClass("active");
     })
+
+    // typing animation script
+
+    var typed = new Typed(".typing", {
+        strings: ["Développeur", "Informaticien"],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true
+    })
+
+    var typed = new Typed(".typing-2", {
+        strings: ["Développeur", "Informaticien"],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true
+    })
+
 
     // owl carousel script
 
